ac2080cba0cfdf50ce8da4dc2655988d594a95ec
Bucky <jacobowens75@gmail.com>
2025-02-20 07:50:35 -0600

    changed dockerFile and fly.toml to use port 5000

--- src/server.ts
+++ src/server.ts
@@ -149,7 +149,7 @@ app.use((err: any, req: any, res: any, next: any) => {
   res.status(500).send("Something broke!")
 })
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 
 // Start the server
 server.listen(PORT, () => {
